45cf5bf6bdf96800c8cb6c5341afa9bdfa3f703a
libishm <96239085+libishm@users.noreply.github.com>
2023-02-01 14:03:37 +0100

    Update sequencer.py

--- ur10e_examples/examples/sequencer.py
+++ ur10e_examples/examples/sequencer.py
@@ -11,12 +11,26 @@ from pilz_robot_program.pilz_robot_program import (Circ, Lin, Ptp, Sequence,
 
 # define robot poses
 home = (0.0, -pi / 2.0, pi / 2.0, 0.0, pi / 2.0, -pi / 2.0)
-pose_l = Pose(position=Point(0.6, -0.6, 0.4),
+pose_l = Pose(position=Point(0.6, -0.4, 0.3),
               orientation=from_euler(0.0, pi, 0.0))
-pose_r = Pose(position=Point(0.6, 0.6, 0.4),
+pose_r = Pose(position=Point(0.6, 0.4, 0.3),
               orientation=from_euler(0.0, pi, 0.0))
-poses = [home, pose_l, pose_r]
+pose_l1 = Pose(position=Point(0.7, 0.4, 0.3),
+              orientation=from_euler(0.0, pi, 0.0))
+pose_r2 = Pose(position=Point(0.7, -0.4, 0.3),
+              orientation=from_euler(0.0, pi, 0.0))
 
+
+poses = [home, pose_l, pose_r, pose_l1, pose_r2]
+
+# place_poses = [Pose(position=Point(0.5, -0.4, 0.3),
+#                     orientation=from_euler(0.0, pi, 0.0)),
+#                Pose(position=Point(0.5, 0.4, 0.3),
+#                     orientation=from_euler(0.0, pi, 0.0)),
+#                Pose(position=Point(0.6, 0.4, 0.3),
+#                     orientation=from_euler(0.0, pi, 0.0)),
+#                Pose(position=Point(0.6, -0.4, 0.3),
+#                     orientation=from_euler(0.0, pi, 0.0))]
 
 def robot_program():
 
@@ -30,19 +44,27 @@ def robot_program():
     mgi.add_ground_cube()
 
     # display pose markers in rviz
-    mgi.publish_pose_array([pose_l, pose_r])
+    mgi.publish_pose_array([pose_l, pose_r, pose_l1, pose_r2])
 
     # plan to home position
     success, plan = mgi.sequencer.plan(
-        Ptp(goal=home, vel_scale=0.3, acc_scale=0.3))[:2]
-    if not success:
+        Lin(goal=home, vel_scale=0.3, acc_scale=0.3))[:2]
+    if not success: 
         return rospy.logerr('Failed to plan to home position')
     mgi.sequencer.execute(plan)
 
     # plan to pose
     for pose in poses:
         success, plan = mgi.sequencer.plan(
-            Ptp(goal=pose, vel_scale=0.3, acc_scale=0.3))[:2]
+            Lin(goal=pose, vel_scale=0.3, acc_scale=0.3))[:2]
+           
+        if not success:
+            return rospy.logerr('Failed to plan to pose')
+        mgi.sequencer.execute()
+    # for pose in poses:
+    #     success, plan = mgi.sequencer.plan(
+    #         Lin(goal=place_poses, vel_scale=0.3, acc_scale=0.3))[:2]
+           
         if not success:
             return rospy.logerr('Failed to plan to pose')
         mgi.sequencer.execute()
